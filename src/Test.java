95d9bdbea9606ec4d5eb5fddb12053d3c36fbd98
kosarep <grzesiek.kosicki@gmail.com>
2017-02-26 17:18:40 +0100

    First commit

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Grzegorz on 26.02.2017.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.print("Pierwsza zmiana i commit.");
+        System.out.print("Druga zmiana i commit.");
+    }
+}
